e07c823806f1908311cb7afb5e12eb99adf26bbf
sureshnandi <34992897+sureshnandi@users.noreply.github.com>
2021-08-05 17:20:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   access_key = var.taccess_key
   secret_key = var.tsecret_key
 }
-resource "aws_instance" "myec2" {
+resource "aws_instance" "mydemo" {
   ami = "ami-0c2b8ca1dad447f8a"
   instance_type = "t2.micro"
 }
