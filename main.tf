c383c81c09d40bb6364148e754b6cc59385dbc0f
sureshnandi <34992897+sureshnandi@users.noreply.github.com>
2021-08-05 17:22:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,3 @@ resource "aws_instance" "mydemo" {
   ami = "ami-0c2b8ca1dad447f8a"
   instance_type = "t2.micro"
 }
-variable "taccess_key"{
-  default = "AKIA5OKHV6HXAT3VABWT"
-}
-variable "tsecret_key"{
-  default = "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"
-}
