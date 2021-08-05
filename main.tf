60ef025440df3ee3d1ab0b48d4a21e7a3c38607b
Suresh Nandi <Suresh_Nandi@epam.com>
2021-08-05 17:06:39 +0530

    modify main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secrey_key
 }
 resource "aws_instance" "myec2" {
   ami = "ami-0c2b8ca1dad447f8a"
   instance_type = "t2.micro"
+}
+variable "taccess_key"{
+  default = "AKIA5OKHV6HXAT3VABWT"
+}
+variable "tsecret_key"{
+  default = "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"
 }
