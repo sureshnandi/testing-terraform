cd3deec558baef533938f016aae3b01164adb3da
sureshnandi <34992897+sureshnandi@users.noreply.github.com>
2021-08-05 17:17:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"https://github.com/sureshnandi/testing-terraform/pulls
   access_key = var.taccess_key
-  secret_key = var.tsecrey_key
+  secret_key = var.tsecret_key
 }
 resource "aws_instance" "myec2" {
   ami = "ami-0c2b8ca1dad447f8a"
